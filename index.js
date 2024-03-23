d757dbc393c4dd036d279074ceda1e79f32c8e36
CarlMathew <moradacarl2711@gmail.com>
2024-03-23 12:36:15 +0800

    All files that pertains to the ecommerce Site
    I already all the images and the design for html and css
    
    I also included the javascript file but it's not finish yet.

---
+++ index.js
@@ -0,0 +1,16 @@
+var swiper1 = new Swiper(".Slider", {
+    effect:"cards",
+    grabCursor: true,
+   
+    loop: true
+})
+var swiper2 = new Swiper(".Slider2", {
+    effect: "cards",
+    loop: true,
+    grabCursor: true
+})
+var swiper3 = new Swiper(".Slider3", {
+    effect: "cards",
+    loop: true,
+    grabCursor: true
+})
